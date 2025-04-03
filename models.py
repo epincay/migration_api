063953e22b8dd503d77d2789a2dd17f38af0b08f
eddie.pincay <eddie.pincay@compsesa.com.ec>
2025-04-03 15:49:59 -0500

    Arreglo para endpoint employees

--- models.py
+++ models.py
@@ -21,5 +21,5 @@ class Employee(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=False, index=True)
     date = Column(DateTime, unique=False, index=True)
-    department_id = Column(Integer, ForeignKey('departments.id'))
-    job_id = Column(Integer, ForeignKey('jobs.id'))
+    department_id = Column(Integer)
+    job_id = Column(Integer)
